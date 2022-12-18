898490a3c7aa7a661bf7c871fe40dcb79c73e730
Cezara Borsan <cezarabo@gmail.com>
2022-12-18 12:34:15 +0200

    create html, css and js file. create navbar. create hero section, projects section, contact section. add darkmode.

---
+++ app.js
@@ -0,0 +1,57 @@
+const loadText = document.querySelector(".loading-text");
+const container = document.querySelector(".container");
+const scollTop = document.querySelector(".up-arrow");
+
+// let load = 0;
+
+// let loaderInterval = setInterval(bluring);
+
+// Intersection Observer, section pop up when scoll into view
+
+const target = document.querySelectorAll(".translate");
+const body = document.querySelector("body");
+
+const options = {
+  rootMargin: "-20px 0px 0px",
+};
+
+const translateOnScroll = new IntersectionObserver(function (
+  entries,
+  translateOnScroll
+) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
+    }
+  });
+},
+options);
+
+target.forEach((section) => {
+  translateOnScroll.observe(section);
+});
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+scollTop.addEventListener("click", (e) => {
+  e.preventDefault();
+  topFunction();
+});
+
+// DARK MODE
+
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+const disableDarkMode = () => {
+  document.body.classList.remove("darkmode");
+};
+
+darkModeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("darkmode");
+});
